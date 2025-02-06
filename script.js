87df97d2ac07afbd7e5d934bc379fbd6306fa07a
ToNaza <misterket37@gmail.com>
2025-02-06 22:25:57 +0200

    fix bug

--- script.js
+++ script.js
@@ -54,6 +54,6 @@ document.getElementById("con1").addEventListener("click", function () {
   setTimeout(function () {
     const pElement = document.querySelector("#val_box p"); // Знаходимо <p> в #val_box
     pElement.style.fontSize = "30px"; // Змінюємо розмір шрифта на 30px
-  }, 5200); // Затримка перед зміною шрифта, можна змінити час
+  }, 5300); // Затримка перед зміною шрифта, можна змінити час
   
 });
